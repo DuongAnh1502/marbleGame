631f6e43a7b4005c39eba6d10a56a6b0312c5c23
duonganh203 <rankill52@gmail.com>
2023-10-30 17:37:17 +0700

    challenge

--- Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -6,25 +6,28 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public GameObject powerupPrefab;
-
+    private EnemyX enemy;
     private float spawnRangeX = 10;
     private float spawnZMin = 15; // set min spawn Z
     private float spawnZMax = 25; // set max spawn Z
-
     public int enemyCount;
     public int waveCount = 1;
+    private float enemySpeed;
 
-
-    public GameObject player; 
-
+    public GameObject player;
+    void Start()
+    {
+        enemy = enemyPrefab.GetComponent<EnemyX>();
+        enemySpeed = 0.25f;
+    }
     // Update is called once per frame
     void Update()
     {
-        enemyCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
-
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
         if (enemyCount == 0)
         {
             SpawnEnemyWave(waveCount);
+            enemySpeed += 0.25f;
         }
 
     }
@@ -40,6 +43,7 @@ public class SpawnManagerX : MonoBehaviour
 
     void SpawnEnemyWave(int enemiesToSpawn)
     {
+        enemy.speed = enemySpeed;
         Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
 
         // If no powerups remain, spawn a powerup
@@ -47,9 +51,8 @@ public class SpawnManagerX : MonoBehaviour
         {
             Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
         }
-
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
